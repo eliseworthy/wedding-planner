4e6f527ef1b6b22cdf435a8300c9144afe0575ab
Elise Worthy <elise.worthy@livingsocial.com>
2012-06-19 22:43:12 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeddingPlanner::Application.config.secret_token = 'e30e228ddc5eff25f740c81a8f74f98e4189724349d9b02cf2a58677a290180886f25130dd375a08cd9b11a310b581ce64cebdf2fb9cb340fe69bef1fa809e4f'
