e3878c211509a4dff829cabf374d4fe1fd812942
Elise Worthy <elise.worthy@livingsocial.com>
2012-06-19 23:40:58 -0400

    set up factories. need to consider photos and users before proceeding

--- spec/controllers/weddings_controller_spec.rb
+++ spec/controllers/weddings_controller_spec.rb
@@ -1,8 +1,23 @@
 require 'spec_helper'
 
 describe WeddingsController do
-  it "renders an index" do
-    get :index 
-    response.should be_successful
+  let(:wedding) { FactoryGirl.create(:wedding) }
+
+  context 'index' do
+    before(:each) do
+      wedding
+      get :index
+    end
+    it 'renders an index' do
+      response.should be_successful
+    end
+
+    it 'shows all wedding name, description, and user_id' do
+      response.body.should eq [{ 
+        "id" => wedding.id,
+        "name" => wedding.name,
+        "description" => wedding.description
+      }]
+    end
   end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :wedding do
+    name "Wedding Name"
+    description "A great zombie"
+    user_id 1
+  end
+end
