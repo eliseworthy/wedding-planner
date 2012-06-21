dafe7f0735bc5e2be2f284f7a82dad930b1ae6f1
Elise Worthy <elise.worthy@livingsocial.com>
2012-06-21 13:59:26 -0400

    added wedding error handling

--- app/controllers/weddings_controller.rb
+++ app/controllers/weddings_controller.rb
@@ -3,10 +3,17 @@ class WeddingsController < ApplicationController
 
   def index
     @weddings = Wedding.all
+    if @weddings.empty?
+      render json: {error: "No weddings found"}, status: :not_found
+    else 
+      @weddings  
+    end
   end
 
   def show
-    @wedding = Wedding.find(params[:id])
+    if @wedding = Wedding.find_by_id(params[:id])
+    else render json: {error: "Wedding not found"}, status: :not_found
+    end
   end
 
   def create
@@ -15,24 +22,26 @@ class WeddingsController < ApplicationController
     if @wedding.save
       render json: @wedding, status: :created
     else
-      render json: @wedding.errors, status: :unprocessable_entity
+      render json: {error: "Cannot create wedding"}, status: :unprocessable_entity
     end
   end
 
   def update
-    @wedding = Wedding.find(params[:id])
-
-    if @wedding.update_attributes(params[:wedding])
-      head :no_content
-    else
-      render json: @wedding.errors, status: :unprocessable_entity
-    end
+    if @wedding = Wedding.find_by_id(params[:id])
+      if @wedding.update_attributes(params[:wedding])
+        head :no_content
+      else
+        render json: {error: "Cannot update wedding"}, status: :unprocessable_entity
+      end
+    else render json: {error: "Cannot find wedding, unable to update"}, status: :unprocessable_entity
+    end  
   end
 
   def destroy
-    @wedding = Wedding.find(params[:id])
-    @wedding.destroy
-
-    head :no_content
+    if @wedding = Wedding.find_by_id(params[:id])
+      @wedding.destroy
+      head :no_content
+    else render json: {error: "Cannot find wedding, unable to delete"}, status: :unprocessable_entity
+    end
   end
 end
--- spec/controllers/weddings_controller_spec.rb
+++ spec/controllers/weddings_controller_spec.rb
@@ -3,84 +3,169 @@ require 'spec_helper'
 describe WeddingsController do
   render_views
 
-  let!(:wedding) { FactoryGirl.create(:wedding) }
+  let(:wedding) { FactoryGirl.create(:wedding) }
 
   context 'index' do
-    before(:each) do
-      get :index, :format => :json
+    context 'with at least 1 wedding' do
+      before(:each) do
+        wedding
+        get :index, :format => :json
+      end
+
+      it 'renders an index' do
+        response.should be_success
+      end
+
+      it 'returns an array of weddings' do
+        body = JSON.parse(response.body)
+        body["weddings"].should eq [{ 
+          "id" => wedding.id,
+          "name" => wedding.name,
+          "description" => wedding.description,
+          "user_id" => wedding.user_id,
+          "created_at" => wedding.created_at.strftime('%FT%TZ')
+        }]
+      end
     end
 
-    it 'renders an index' do
-      response.should be_success
-    end
+    context 'with no weddings' do
+      before(:each) do
+        get :index, :format => :json
+      end
 
-    it 'returns an array of weddings' do
-      body = JSON.parse(response.body)
-      body["weddings"].should eq [{ 
-        "id" => wedding.id,
-        "name" => wedding.name,
-        "description" => wedding.description,
-        "user_id" => wedding.user_id,
-        "created_at" => wedding.created_at.strftime('%FT%TZ')
-      }]
+      it 'has a 404 error code' do
+        response.status.should == 404
+      end
+
+      it 'returns an error message' do
+        body = JSON.parse(response.body)
+        body["error"].should == "No weddings found"
+      end
     end
   end
+
   context 'show' do
-    before(:each) do
-      wedding
-      get :show, format: :json, id: wedding.id
+    context 'correct parameters' do
+      before(:each) do
+        wedding
+        get :show, format: :json, id: wedding.id
+      end
+
+      it 'renders the show' do
+        response.should be_success
+      end
+
+      it 'returns a wedding hash' do
+        body = JSON.parse(response.body)
+        body.should == { 
+          "id" => wedding.id,
+          "name" => wedding.name,
+          "description" => wedding.description,
+          "user_id" => wedding.user_id,
+          "created_at" => wedding.created_at.strftime('%FT%TZ')
+        }
+      end
     end
 
-    it 'renders the show' do
-      response.should be_success
-    end
+    context 'incorrect parameters' do
+      before(:each) do
+        get :show, format: :json, id: 1
+      end
 
-    it 'returns a wedding hash' do
-      body = JSON.parse(response.body)
-      body.should == { 
-        "id" => wedding.id,
-        "name" => wedding.name,
-        "description" => wedding.description,
-        "user_id" => wedding.user_id,
-        "created_at" => wedding.created_at.strftime('%FT%TZ')
-      }
+      it 'return an error code' do
+        response.status.should == 404
+      end
+
+      it 'returns an error message' do
+        body = JSON.parse(response.body)
+        body["error"].should ==  "Wedding not found"
+      end
     end
   end
 
   context 'create' do
-    it "should create a new wedding" do
-      expect {
-        post :create, :wedding => { name: 'Pretty wedding', description: 'Pretty!', user_id: 1}
-      }.to change(Wedding, :count).by 1
+    context "correct parameters" do
+      it "should create a new wedding" do
+        expect {
+          post :create, :wedding => { name: 'Pretty wedding', description: 'Pretty!', user_id: 1}
+        }.to change(Wedding, :count).by 1
+      end
+    end
+    context "incorrect parameters" do
+      before(:each) do
+        post :create, :wedding => { name: nil, description: 'Pretty!', user_id: 1}
+      end
+
+      it "returns an error code" do
+        response.status.should == 422
+      end
+
+      it 'returns an error message' do
+        body = JSON.parse(response.body)
+        body["error"].should ==  "Cannot create wedding"
+      end
     end
   end
 
   context 'update' do
-    it "locates the wedding" do
-      put :update, id: wedding, wedding: Factory.attributes_for(:wedding)
-      assigns(:wedding).should eq(wedding) 
-    end
+    context 'correct attributes' do
+      it "locates the wedding" do
+        put :update, id: wedding, wedding: Factory.attributes_for(:wedding)
+        assigns(:wedding).should eq(wedding) 
+      end
 
-    it "updates wedding's attributes" do
-      put :update, id: wedding, wedding: Factory.attributes_for(:wedding, description: "Cool!")
-      wedding.reload
-      wedding.description.should eq("Cool!")
-    end
+      it "updates wedding's attributes" do
+        put :update, id: wedding, wedding: Factory.attributes_for(:wedding, description: "Cool!")
+        wedding.reload
+        wedding.description.should eq("Cool!")
+      end
+    end  
 
     context "invalid attributes" do
-      it "does not change the wedding's attributes" do
+      before(:each) do
         put :update, id: wedding, wedding: Factory.attributes_for(:wedding, description: nil)
+      end
+
+      it "does not change the wedding's attributes" do
         wedding.reload
         wedding.description.should eq("A great zombie")
+      end
+
+      it 'returns an error code' do
+        response.status.should == 422
+      end
+
+      it 'returns an error message' do
+        body = JSON.parse(response.body)
+        body['error'].should == "Cannot update wedding"
       end
     end
   end
 
   context 'destroy' do
-    it "deletes the contact" do
-      expect{
-        delete :destroy, id: wedding       
-      }.to change(Wedding, :count).by -1
+    context 'correct parameters' do
+      it "deletes the contact" do
+        wedding
+        expect{
+          delete :destroy, id: wedding       
+        }.to change(Wedding, :count).by -1
+      end
+    end
+
+    context 'invalid parameters' do
+      before(:each) do
+        wedding
+        delete :destroy, id: wedding.id + 1
+      end
+
+      it 'returns an error code' do
+        response.status.should == 422
+      end
+
+      it 'returns an error message' do
+        body = JSON.parse(response.body)
+        body["error"].should == "Cannot find wedding, unable to delete"
+      end
     end
   end
 end
