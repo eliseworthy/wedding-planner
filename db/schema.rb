b822026e8b58d739116c3d0352385e865effe24a
Elise Worthy <elise.worthy@livingsocial.com>
2012-07-02 17:06:14 -0400

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120628193830) do
+ActiveRecord::Schema.define(:version => 20120702210235) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(:version => 20120628193830) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "user_keys", :force => true do |t|
+    t.string   "api_key"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "weddings", :force => true do |t|
